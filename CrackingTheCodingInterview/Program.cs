b0ac17255924cca5c440ea9268dbf3aa53afc499
gergomahler <49788003+gergomahler@users.noreply.github.com>
2020-10-29 13:27:14 +0100

    Add project files.

---
+++ CrackingTheCodingInterview/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingTheCodingInterview
+{
+    class Program
+    {
+        static ch1 ch1 = new ch1();
+        static void Main(string[] args)
+        {
+            //1.1
+            Console.WriteLine(ch1.IsUnique("aabb"));
+            Console.WriteLine(ch1.IsUnique("abcd"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/ch1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingTheCodingInterview
+{
+    class ch1
+    {
+            // 1.1 IsUnique
+
+            public bool IsUnique(string str)
+            {
+                HashSet<char> charSet = new HashSet<char>();
+                foreach (char c in str)
+                {
+                    charSet.Add(c);
+                }
+                return charSet.Count == str.Length;
+            }
+
+    }
+}
